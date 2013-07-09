b63a97df4b3c90837a881c79040d2ff58fb6d1f9
Ubuntu <ubuntu@ip-172-31-44-196.us-west-2.compute.internal>
2013-07-09 22:52:55 +0000

    Updated grader.js to take URL

--- grader.js
+++ grader.js
@@ -65,8 +65,15 @@ if(require.main == module) {
     program
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+	.option('-u, --url<url>', 'Website URL')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    if(program.file){
+	var checkJson = checkHtmlFile(program.file, program.checks);
+    } else if(program.url){
+	rest.get(program.url).on('success',function(data){
+	    var checkJson = checkHtmlFile(data,program.checks);
+	    });
+      }
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
